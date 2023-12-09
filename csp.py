83458cb532ac47e66f6aeac237130f9faeff0c3a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2023-12-09 16:04:31 +1000

    UPDATE: Smooth code refactor

--- csp.py
+++ csp.py
@@ -1,9 +1,18 @@
-from flask import Flask, make_response, request, render_template, send_from_directory
-import os.path
+"""
+    Wr3dmast3r lab
+"""
+
+from flask import (
+    Flask,
+    make_response,
+    request,
+    render_template,
+    send_from_directory
+)
 
 
 app = Flask(__name__)
-global csps
+
 csps = {
     1: "script-src 'self' data:; object-src 'none'",
     2: "script-src https://google.com 'unsafe-inline';",
@@ -12,7 +21,7 @@ csps = {
     5: "script-src 'self' https://www.google.com; object-src 'none';",
     6: "script-src 'self'; object-src 'none'",
     7: "script-src https://google.com 'unsafe-eval';",
-    8: "script-src 'self' https://google.com https: data *;" ,
+    8: "script-src 'self' https://google.com https: data *;",
     9: "script-src https://cdnjs.cloudflare.com 'unsafe-eval';",
     10: "default-src 'self'; connect-src 'self'; script-src 'self';",
     11: "script-src 'self' ajax.googleapis.com; object-src 'none' ;report-uri /Report-parsing-url;",
@@ -21,31 +30,56 @@ csps = {
 
 
 @app.get("/lab/<int:id>/search")
-def search(id):
+def search(search_id):
+    """_summary_
+
+    Args:
+        search_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
     item = request.args.get("search")
     response = make_response(f"<h2>'{item}' not found</h2>")
-    csp = csps.get(id)
+    csp = csps.get(search_id)
 
     if csp is not None:
         response.headers["Content-Security-Policy"] = csp
+
     return response
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
     res = make_response(render_template("index.html", csps=csps))
+
     if request.args.get('csp'):
         csps[len(csps)+1] = request.args.get('csp')
         res = make_response(render_template("index.html", csps=csps))
 
     return res
+
+
 @app.get("/uploads/<path:filename>")
 def uploads(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
     return send_from_directory("C:\\tmp\\uploads", filename)
 
 
 if __name__ == "__main__":
     app.run("127.0.0.1", "8001")
-
-
-
-
